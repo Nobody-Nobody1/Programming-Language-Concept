1a21258f23cfc139030e26300fd832b4307c8b59
Nobody_Nobody1 <bibhasn123@gmail.com>
2025-12-19 03:49:56 +0000

    modified:   ByteCode.txt 	modified:   VMPython.py 	modified:   __pycache__/ByteCodeReader.cpython-312.pyc 	modified:   __pycache__/VMPython.cpython-312.pyc

--- VMPython.py
+++ VMPython.py
@@ -1,13 +1,14 @@
 import ByteCodeReader
 
 class SimpleVM:
-    def __init__self(self):
+    def __init__(self):
         self.stack = []
+        self.program = 0
     
     def execute(self, bytecode):
         for instruction in bytecode:
             if instruction == "PUSH": #adds next value from bytecode to stack
-                value = bytecode.pop(0)
+                value = bytecode[self.program + 1]
                 self.stack.append(value)
             elif instruction == "POP": #removes top value from stack
                 self.stack.pop()
@@ -20,9 +21,10 @@ class SimpleVM:
                 a = self.stack.pop()
                 self.stack.append(a * b)
             elif instruction == "PRINT": # prints top value of stack
-                value = self.stack
-                print(value)
+                print("Print :)")
             elif instruction == "PROGRAM_CHECK": # prints program check message
                 print("Program Check :)")
             else:
                 raise ValueError(f"Unknown instruction: {instruction}") #raises error for unknown
+        else:
+            return self.stack #returns final stack after execution
