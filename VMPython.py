c0b2cb27fe7fd80a5255783bbd412bf4cd30735b
Nobody_Nobody1 <bibhasn123@gmail.com>
2025-12-27 10:49:00 -0800

    new file:   .vscode/launch.json 	modified:   ByteCode.txt 	modified:   VMPython.py 	new file:   __pycache__/ByteCodeReader.cpython-313.pyc 	new file:   __pycache__/ReadmeInteracter.cpython-313.pyc 	new file:   __pycache__/VMPython.cpython-313.pyc

--- VMPython.py
+++ VMPython.py
@@ -9,7 +9,7 @@ class SimpleVM:
         
         bytecode_length = len(bytecode)
 
-        while self.program <= bytecode_length - 1:
+        while self.program <= bytecode_length:
 
             instruction = bytecode[self.program]
 
@@ -21,7 +21,7 @@ class SimpleVM:
                 
                 
 
-            elif instruction == 0x20 or "ADD":  # ADD instruction or 32
+            elif instruction == "ADD":  # ADD instruction or 32
                 if len(self.stack) >= 2:
                     b = self.stack.pop()
                     a = self.stack.pop()
@@ -30,7 +30,7 @@ class SimpleVM:
                 else:
                     raise ValueError("Stack underflow in ADD instruction")
                 
-            elif instruction == 0x30 or "SUB":  # SUB instruction or 48
+            elif instruction == "SUB":  # SUB instruction or 48
                 if len(self.stack) >= 2:
                     b = self.stack.pop()
                     a = self.stack.pop()
@@ -39,7 +39,7 @@ class SimpleVM:
                 else:
                     raise ValueError("Stack underflow in SUB instruction")
                 
-            elif instruction == 0x40 or "MUL":  # MUL instruction or 64
+            elif instruction == "MUL":  # MUL instruction or 64
                 if len(self.stack) >= 2:
                     b = self.stack.pop()
                     a = self.stack.pop()
@@ -48,7 +48,7 @@ class SimpleVM:
                 else:
                     raise ValueError("Stack underflow in MUL instruction")
                 
-            elif instruction == 0x50 or "DIV":  # DIV instruction or 80
+            elif instruction == "DIV":  # DIV instruction or 80
                 if len(self.stack) >= 2:
                     b = self.stack.pop()
                     a = self.stack.pop()
@@ -60,21 +60,21 @@ class SimpleVM:
                 else:
                     raise ValueError("Stack underflow in DIV instruction")
                 
-            elif instruction == 0x60 or "HALT":  # HALT instruction or 96
+            elif instruction == "HALT":  # HALT instruction or 96
                 break
 
-            elif instruction == 0x70 or "PRINT":  # PRINT instruction or 112
+            elif instruction == "PRINT":  # PRINT instruction or 112
                 if self.stack:
                     print(self.stack.pop())
                     self.program += 1
                 else:
                     raise ValueError("Stack underflow in PRINT instruction")
                 
-            elif instruction == 0x80 or "CLEAR":  # CLEAR instruction or 128
+            elif instruction == "CLEAR":  # CLEAR instruction or 128
                 self.stack.clear()
                 self.program += 1
             
-            elif instruction == 0x90 or "STORE":  # STORE instruction or 144
+            elif instruction == "STORE":  # STORE instruction or 144
                 if self.stack:
                     value = self.stack.pop()
                     with open('vm_store.txt', 'w') as f:
@@ -83,7 +83,7 @@ class SimpleVM:
                 else:
                     raise ValueError("Stack underflow in STORE instruction")
                 
-            elif instruction == 0xA0 or "LOAD":  # LOAD instruction or 160
+            elif instruction == "LOAD":  # LOAD instruction or 160
                 try:
                     with open('vm_store.txt', 'r') as f:
                         value = int(f.read())
@@ -92,11 +92,11 @@ class SimpleVM:
                 except FileNotFoundError:
                     raise FileNotFoundError("No stored value found for LOAD instruction")
 
-            elif instruction == 0xB0 or "JUMP": # JUMP instruction or 176
+            elif instruction == "JUMP": # JUMP instruction or 176
                 target = bytecode[self.program + 1]
                 self.program = target
 
-            elif instruction == 0xC0 or "JUMPIFZERO": # JUMPIFZERO instruction or 192
+            elif instruction == "JUMPIFZERO": # JUMPIFZERO instruction or 192
                 if self.stack:
                     value = self.stack.pop()
                     target = bytecode[self.program + 1]
@@ -107,7 +107,7 @@ class SimpleVM:
                 else:
                     raise ValueError("Stack underflow in JUMPIFZERO instruction")
             
-            elif instruction == 0xD0 or "HELP": # HELP instruction or 208
+            elif instruction == "HELP": # HELP instruction or 208
                 readme = ReadmeInteracter.display_readme()
                 print(readme)
                 break
