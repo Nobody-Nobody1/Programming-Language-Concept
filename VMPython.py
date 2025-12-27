eb6424d448bd5ab223ac2b3366d07a93414778f4
Nobody_Nobody1 <bibhasn123@gmail.com>
2025-12-27 10:32:57 -0800

    modified:   VMPython.py

--- VMPython.py
+++ VMPython.py
@@ -13,7 +13,7 @@ class SimpleVM:
 
             instruction = bytecode[self.program]
 
-            if instruction == 0x10 or "PUSH":  # PUSH instruction or 16
+            if instruction == "PUSH":  # PUSH instruction or 16
                 print("hello" + str(self.program))
                 self.program += 1
                 value = bytecode[self.program]
