29868b31865ffec6e59ec9e0add2bdd95d989142
Nobody_Nobody1 <bibhasn123@gmail.com>
2025-12-30 15:14:06 -0800

    modified:   VMPython.py

--- VMPython.py
+++ VMPython.py
@@ -27,6 +27,35 @@ class SimpleVM:
                     self.stack.append(a + b)
                 else:
                     raise ValueError("Stack underflow in ADD instruction")
+            elif instruction == "SUB":  # SUB instruction
+                if len(self.stack) >= 2:
+                    b = self.stack.pop()
+                    a = self.stack.pop()
+                    self.stack.append(a - b)
+                else:
+                    raise ValueError("Stack underflow in SUB instruction")
+            elif instruction == "MUL":  # MUL instruction
+                if len(self.stack) >= 2:
+                    b = self.stack.pop()
+                    a = self.stack.pop()
+                    self.stack.append(a * b)
+                else:
+                    raise ValueError("Stack underflow in MUL instruction")
+            elif instruction == "DIV":  # DIV instruction
+                if len(self.stack) >= 2:
+                    b = self.stack.pop()
+                    a = self.stack.pop()
+                    if b != 0:
+                        self.stack.append(a // b)
+                    else:
+                        raise ValueError("Division by zero in DIV instruction")
+                else:
+                    raise ValueError("Stack underflow in DIV instruction")
+                
+            elif instruction == "JUMP":  # JUMP instruction
+                self.instruction_pointer += 1
+                target = bytecode[self.instruction_pointer]
+                self.instruction_pointer = target - 1  # -1 to offset the upcoming increment
 
             elif instruction == "JUMPIFZERO":  # JUMPIFZERO instruction
                 self.instruction_pointer += 1
@@ -37,6 +66,16 @@ class SimpleVM:
                         self.instruction_pointer = target - 1  # -1 to offset the upcoming increment
                 else:
                     raise ValueError("Stack underflow in JUMPIFZERO instruction")
+            
+            elif instruction == "PRINT":  # PRINT instruction
+                if self.stack:
+                    value = self.stack.pop()
+                    print(value)
+                else:
+                    raise ValueError("Stack underflow in PRINT instruction")
+            
+            elif instruction == "HALT":  # HALT instruction
+                break
             
             elif instruction == "HELP": # HELP instruction or 208
                 readme = ReadmeInteracter.display_readme()
